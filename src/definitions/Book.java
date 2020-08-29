84b407cf4f69b4c117f7921d61d08d599c38a979
Harshita077 <harshita.shukla_cs19@gla.ac.in>
2020-08-30 00:12:40 +0530

    create class Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -8,7 +8,7 @@ package definitions;
 
 import java.util.Objects;
 
-public class Book {
+public class Book{
     private String BookName;
     private String BookAuthour;
     private String ISBNnoofBook;

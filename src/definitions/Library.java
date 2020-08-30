1486406424272f8642bf1f91683ee34f70a367c1
Harshita077 <harshita.shukla_cs19@gla.ac.in>
2020-08-30 16:30:56 +0530

    create scanner object

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -7,6 +7,57 @@ public class Library {
 
     //Created the Constuctor
     public Library() {
+        this.currentlyAvailableBooks = new Book[10];
 
+
+        this.currentlyAvailableBooks[0] = new Book("The Blue Umbrella", "Ruskin Bond", "7536598493435");
+        this.currentlyAvailableBooks[1] = new Book("The Tempest", "William Shakespeare", "8753920278842");
+        this.currentlyAvailableBooks[2] = new Book("Treasure Trove", "Michael Stadther", "3490267835203");
+    }
+
+    public Book[] getCurrentlyAvailableBooks() {
+        return currentlyAvailableBooks;
+    }
+
+    public void setCurrentlyAvailableBooks(Book[] currentlyAvailableBooks) {
+        this.currentlyAvailableBooks = currentlyAvailableBooks;
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "currentlyAvailableBooks=" + Arrays.toString(currentlyAvailableBooks) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Library)) return false;
+        Library library = (Library) o;
+        return Arrays.equals(getCurrentlyAvailableBooks(), library.getCurrentlyAvailableBooks());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getCurrentlyAvailableBooks());
+    }
+
+    public void addBooks(String bookName) {
+        System.out.println(bookName + ",is added Successfully");
+    }
+
+    public void doCheckOut(String bookName) {
+        System.out.println(bookName + ",is issued Successfully");
+    }
+
+    public void doReturn(String bookName) {
+        System.out.println(bookName + " is returned Successfully");
+    }
+
+    public void listBooks() {
+        for (Book bookObject : this.currentlyAvailableBooks) {
+            System.out.println();
+        }
     }
 }
--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -5,3 +5,19 @@ import definitions.Book;
 import definitions.Library;
 import definitions.Student;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class FrontDesk{
+    public static final int EXIT=4;
+    public static final int ISSUE_A_BOOK =1;
+    public static final int RETURN_PREVIOUSLY_ISSUED_BOOKS=2;
+    public static final int SHOW_ALL_BOOKS_ISSUED=3;
+
+    public static void main(String[] args) {
+        Scanner scannerObject = new Scanner(System.in);
+
+
+    }
+}
+

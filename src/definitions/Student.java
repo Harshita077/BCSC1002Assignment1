a7b0cbefc257571fe08be74579a0d27d4f042d0e
Harshita077 <harshita.shukla_cs19@gla.ac.in>
2020-08-30 12:16:18 +0530

    create hashcode method for Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -24,4 +24,72 @@ public class Student {
         this.universityRollnoOfStudent=universityRollnoOfStudent;
         this.totalNoOfBooksIssued=totalNoOfBooksIssued;
     }
+
+    public Student () {
+        this.nameOfBooksIssued = new Book[15];
+    }
+
+    public String getFirstNameOfStudent() {
+        return firstNameOfStudent;
+    }
+
+    public void setFirstNameOfStudent(String firstNameOfStudent) {
+        this.firstNameOfStudent = firstNameOfStudent;
+    }
+
+    public String getMiddleNameOfStudent() {
+        return middleNameOfStudent;
+    }
+
+    public void setMiddleNameOfStudent(String middleNameOfStudent) {
+        this.middleNameOfStudent = middleNameOfStudent;
+    }
+
+    public String getLastNameOfStudent() {
+        return lastNameOfStudent;
+    }
+
+    public void setLastNameOfStudent(String lastNameOfStudent) {
+        this.lastNameOfStudent = lastNameOfStudent;
+    }
+
+    public long getUniversityRollnoOfStudent() {
+        return universityRollnoOfStudent;
+    }
+
+    public void setUniversityRollnoOfStudent(long universityRollnoOfStudent) {
+        this.universityRollnoOfStudent = universityRollnoOfStudent;
+    }
+
+    public Book[] getNameOfBooksIssued() {
+        return nameOfBooksIssued.clone();
+    }
+
+    public void setNameOfBooksIssued(Book[] nameOfBooksIssued) {
+        this.nameOfBooksIssued = nameOfBooksIssued;
+    }
+    @Override
+    public String toString() {
+        return Arrays.toString(nameOfBooksIssued);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return getUniversityRollnoOfStudent() == student.getUniversityRollnoOfStudent() &&
+                totalNoOfBooksIssued == student.totalNoOfBooksIssued &&
+                Objects.equals(getFirstNameOfStudent(), student.getFirstNameOfStudent()) &&
+                Objects.equals(getMiddleNameOfStudent(), student.getMiddleNameOfStudent()) &&
+                Objects.equals(getLastNameOfStudent(), student.getLastNameOfStudent()) &&
+                Arrays.equals(getNameOfBooksIssued(), student.getNameOfBooksIssued());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(getFirstNameOfStudent(), getMiddleNameOfStudent(), getLastNameOfStudent(), getUniversityRollnoOfStudent(), totalNoOfBooksIssued);
+        result = 31 * result + Arrays.hashCode(getNameOfBooksIssued());
+        return result;
+    }
 }

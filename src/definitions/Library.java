ffcb2a035511ded3ce336d4a2e3a98f7332d5540
Harshita077 <harshita.shukla_cs19@gla.ac.in>
2020-08-30 16:00:12 +0530

    Create the Constructor

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,53 +1,12 @@
-/*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
- *  File Name : Library.java
- * */
 package definitions;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 public class Library {
     private Book[] currentlyAvailableBooks;
 
-    public Library(Book[] currentlyAvailableBooks) {
-        this.currentlyAvailableBooks = currentlyAvailableBooks;
-    }
-
+    //Created the Constuctor
     public Library() {
-        Book bookA = new Book("The Blue Umbrella", "Ruskin Bond", "45865250200021");
-        Book bookB = new Book("The Tempest", "William Shakespeare", "809039390567");
-        Book bookC = new Book("Treasure Trove", "Michael Stadther", "952020827494");
 
-    }
-
-    public Book[] getCurrentlyAvailableBooks() {
-        return currentlyAvailableBooks.clone();
-    }
-
-    public void setCurrentlyAvailableBooks(Book[] currentlyAvailableBooks) {
-        this.currentlyAvailableBooks = currentlyAvailableBooks;
-    }
-
-    @Override
-    public String toString() {
-        return Arrays.toString(currentlyAvailableBooks);
-
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Library)) return false;
-        Library library = (Library) o;
-        return Arrays.equals(getCurrentlyAvailableBooks(), library.getCurrentlyAvailableBooks());
-    }
-
-    @Override
-    public int hashCode() {
-        return Arrays.hashCode(getCurrentlyAvailableBooks());
     }
 }
-
--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -1,27 +1,7 @@
-/*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
- *  File Name : FrontDesk.java
- * */
+
 package execution;
 
 import definitions.Book;
 import definitions.Library;
 import definitions.Student;
 
-import java.util.Scanner;
-import java.util.Arrays;
-
-public class FrontDesk{
-    private static final int EXIT = 4;
-    private static final int  ISSUE_BOOK=1;
-    private static final int  RETURN_PREVIOUSLY_ISSUED_BOOKS=2;
-    private static final int SHOW_ALL_BOOKS_ISSUED =3;
-
-    public static void main(String [] args){
-        Scanner scannerObject = new Scanner(System.in);
-
-    }
-
-}
